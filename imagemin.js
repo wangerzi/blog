a33e9fc362cddde90a4f7195ec273928e3a7f139
Jeffrey-Wang <admin@wj2015.com>
2022-05-11 10:57:42 +0800

    fix: imagemin remove

--- imagemin.js
+++ imagemin.js
@@ -2,14 +2,18 @@ import imagemin from 'imagemin';
 import imageminJpegtran from 'imagemin-jpegtran';
 import imageminPngquant from 'imagemin-pngquant';
 
-const files = await imagemin(['source/static/assets/*.{jpg,png}'], {
-	destination: 'source/static/assets/',
-	plugins: [
-		imageminJpegtran(),
-		imageminPngquant({
-			quality: [0.6, 0.8]
-		})
-	]
-});
+async function handle() {
+    return  await imagemin(['source/static/assets/*.{jpg,png}'], {
+        destination: 'source/static/assets/',
+        plugins: [
+            imageminJpegtran(),
+            imageminPngquant({
+                quality: [0.6, 0.8]
+            })
+        ]
+    });
+}
 
-console.log('handled', files.map(v => v.destinationPath));
+handle().then(files => {
+    console.log('handled', files.map(v => v.destinationPath));
+})
